d985fb1e3b36afcd9f2c17f487cfe97d3cef1eac
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2020-12-21 22:18:25 +0100

    Add functions to change colors of dragons

--- client/assets/js/dragonFactory.js
+++ client/assets/js/dragonFactory.js
@@ -2,36 +2,60 @@
 //Random color
 function getColor() {
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    return randomColor
+    return randomColor;
 }
 
 function genColors(){
-    var colors = []
+    var colors = [];
     for(var i = 10; i < 99; i ++){
-      var color = getColor()
-      colors[i] = color
+      var color = getColor();
+      colors[i] = color;
     }
-    return colors
+    return colors;
 }
 
-// Make a function headcolor with color and code as argument
+// Make a function headBodyColor with a color and code as arguments
 function headBodyColor(color, code) {
     // Change the head and body background color of the dragon
-    $('#head, #body').css('background', '#' + color)
-    // modifies the headcode id with html with the code in the slider
-    $('#headcode').html('code: ' + code)
-    // modifies the dnabody id with html with the code
-    $('#dnabody').html(code)
+    $("#head, #body").css("background", "#" + color);
+    // Modifies the headBodyCode id with the code in the slider
+    $("#headBodyCode").html("code: " + code);
+    // Modifies the headBodyDna id with the code
+    $("#headBodyDna").html(code);
 }
 
-// Make a function headcolor with color and code as argument
+// Make a function wingsTailColor with a color and code as arguments
 function wingsTailColor(color, code) {
-    // Change the head and body background color of the dragon
-    $('#wings, .tail').css('background', '#' + color)
-    // modifies the headcode id with html with the code in the slider
-    $('#wingsTailCode').html('code: ' + code)
-    // modifies the dnabody id with html with the code
-    $('#wingsTailDna').html(code)
+    // Change the wings border left color of the dragon
+    $(".wings").css("border-left-color", "#" + color);
+    // Change the tail background color of the dragon
+    $(".tail").css("background", "#" + color);
+    // Change the tail end border left color of the dragon
+    $(".tailEnd1, .tailEnd2").css("border-left-color", "#" + color);
+    // Modifies the wingsTailCode id with the code in the slider
+    $("#wingsTailCode").html("code: " + code);
+    // Modifies the wingsTailDna id with the code
+    $("#wingsTailDna").html(code);
+}
+
+// Make a function eyesColor with a color and code as arguments
+function eyesColor(color, code) {
+  // Change the pupils background color
+  $(".pupils").css("background", "#" + color);
+  // Modifies eyesCode with the code in the slider
+  $("#eyesCode").html("code: " + code);
+  // Modifies the eyesDna id with the code
+  $("#eyesDna").html(code);
+}
+
+// Make a function legsArmsColor with a color and code as arguments
+function legsArmsColor(color, code) {
+  // Change the legs and arms background color
+  $(".legs, .feet, .arms").css("background", "#" + color)
+  // Modifies the legsArmsCode with the code in the slider
+  $("#legsArmsCode").html("code: " + code);
+  // Modifies the legsArmsDna with the code
+  $("#legsArmsDna").html(code);
 }
 
 
@@ -40,7 +64,7 @@ function wingsTailColor(color, code) {
 //###################################################
 function eyeVariation(num) {
 
-    $('#dnashape').html(num)
+    $('#dnashape').html(num);
     switch (num) {
         case 1:
             normalEyes()
@@ -50,7 +74,7 @@ function eyeVariation(num) {
 }
 
 function decorationVariation(num) {
-    $('#dnadecoration').html(num)
+    $('#dnadecoration').html(num);
     switch (num) {
         case 1:
             $('#decorationName').html('Basic')
@@ -60,13 +84,13 @@ function decorationVariation(num) {
 }
 
 async function normalEyes() {
-    await $('.cat__eye').find('span').css('border', 'none')
+    await $('.cat__eye').find('span').css('border', 'none');
 }
 
 async function normaldecoration() {
     //Remove all style from other decorations
     //In this way we can also use normalDecoration() to reset the decoration style
-    $('.cat__head-dots').css({ "transform": "rotate(0deg)", "height": "48px", "width": "14px", "top": "1px", "border-radius": "0 0 50% 50%" })
-    $('.cat__head-dots_first').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "50% 0 50% 50%" })
-    $('.cat__head-dots_second').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "0 50% 50% 50%" })
+    $('.cat__head-dots').css({ "transform": "rotate(0deg)", "height": "48px", "width": "14px", "top": "1px", "border-radius": "0 0 50% 50%" });
+    $('.cat__head-dots_first').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "50% 0 50% 50%" });
+    $('.cat__head-dots_second').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "0 50% 50% 50%" });
 }
--- client/assets/js/dragonSettings.js
+++ client/assets/js/dragonSettings.js
@@ -1,76 +1,137 @@
-
+// Create a variable that stores the result of the object call
 var colors = Object.values(allColors())
 
+// Make a variable for the default dna object
 var defaultDNA = {
-    "headBodyColor" : 10,
-    "wingsTailColor" : 13,
-    "eyesColor" : 96,
-    "earsColor" : 10,
-    //Cattributes
-    "eyesShape" : 1,
-    "decorationPattern" : 1,
-    "decorationMidcolor" : 13,
-    "decorationSidescolor" : 13,
-    "animation" :  1,
-    "lastNum" :  1
-    }
+  // Dragon colors
+  "headBodyColor": 1,
+  "wingsTailColor": 11,
+  "eyesColor": 21,
+  "legsArmsColor": 31,
+  // Dragon atributes
+  "eyesShape": 1,
+  "decorationPattern": 1,
+  "decorationMidcolor": 13,
+  "decorationSidescolor": 13,
+  "animation": 1,
+  "lastNum": 1
+}
+// Prepare the document
+$(document).ready(function() {
+  // Head and body color id is set
+  $("#headBodyDna").html(defaultDNA.headBodyColor);
+  // Wings and tail color id is set
+  $("#wingsTailDna").html(defaultDNA.wingsTailColor);
+  // Eyes color id is set
+  $("#eyesDna").html(defaultDNA.eyesColor);
+  // Legs and arms color is set
+  $("#legsArmsDna").html(defaultDNA.legsArmsColor);
 
-// when page load
-$( document ).ready(function() {
-  $('#dnabody').html(defaultDNA.headBodyColor);
-  $('#wingsTailDna').html(defaultDNA.wingsTailColor);
-  $('#dnaeyes').html(defaultDNA.eyesColor);
-  $('#dnaears').html(defaultDNA.earsColor);
+  // $('#dnashape').html(defaultDNA.eyesShape);
+  // $('#dnadecoration').html(defaultDNA.decorationPattern);
+  // $('#dnadecorationMid').html(defaultDNA.decorationMidcolor);
+  // $('#dnadecorationSides').html(defaultDNA.decorationSidescolor);
+  // $('#dnaanimation').html(defaultDNA.animation);
+  // $('#dnaspecial').html(defaultDNA.lastNum);
 
-//   $('#dnashape').html(defaultDNA.eyesShape)
-//   $('#dnadecoration').html(defaultDNA.decorationPattern)
-//   $('#dnadecorationMid').html(defaultDNA.decorationMidcolor)
-//   $('#dnadecorationSides').html(defaultDNA.decorationSidescolor)
-//   $('#dnaanimation').html(defaultDNA.animation)
-//   $('#dnaspecial').html(defaultDNA.lastNum)
-
-  renderHeadBody(defaultDNA)
-  renderWingsTail(defaultDNA)
+  // Call function renderHeadBody with the default dna as an argument
+  renderHeadBody(defaultDNA);
+  // Call function renderWingsTail with the default dna as an argument
+  renderWingsTail(defaultDNA);
+  // Call function renderEyes with the default dna as an argument
+  renderEyes(defaultDNA);
+  // Call function renderLegsArms with the default dna as an argument
+  renderLegsArms(defaultDNA);
 });
 
-function getDna(){
-    var dna = ''
-    dna += $('#dnabody').html()
-    dna += $('#wingsTailDna').html()
-    dna += $('#dnaeyes').html()
-    dna += $('#dnaears').html()
-    dna += $('#dnashape').html()
-    dna += $('#dnadecoration').html()
-    dna += $('#dnadecorationMid').html()
-    dna += $('#dnadecorationSides').html()
-    dna += $('#dnaanimation').html()
-    dna += $('#dnaspecial').html()
+// Make a function to get the dna
+function getDna() {
+  // Make a variable dna that is empty
+  var dna = "";
+  // dna += the head and body dna
+  dna += $("#headBodyDna").html();
+  // dna += the wings and tail dna
+  dna += $("#wingsTailDna").html();
+  // dna += eyes dna
+  dna += $("#eyesDna").html();
+  // dna += legs and arms dna
+  dna += $("#legsArmsDna").html();
 
-    return parseInt(dna)
+  // will be used later
+  dna += $("#dnashape").html();
+  dna += $("#dnadecoration").html();
+  dna += $("#dnadecorationMid").html();
+  dna += $("#dnadecorationSides").html();
+  dna += $("#dnaanimation").html();
+  dna += $("#dnaspecial").html();
+  // returns the parseInt dna
+  return parseInt(dna);
 }
 
-function renderHeadBody(dna){
-    headBodyColor(colors[dna.headBodyColor],dna.headBodyColor)
-    $('#bodycolor').val(dna.headBodyColor)
+// Create a function to render the head and body with the dna as an argument
+function renderHeadBody(dna) {
+  // Call function headBodyColor with the color of the head and body color dna
+  // and the head and body color dna as arguments
+  headBodyColor(colors[dna.headBodyColor], dna.headBodyColor);
+  // Sets the head and body color dna in the headBodyColor id
+  $("#headBodyColor").val(dna.headBodyColor);
 }
 
-function renderWingsTail(dna){
-    wingsTailColor(colors[dna.wingsTailColor],dna.wingsTailColor)
-    $('#wingsTailColor').val(dna.wingsTailColor)
+// create a function to render the wings and the tail with the dna as the argument
+function renderWingsTail(dna) {
+  // Call function wingsTailColor with the color of the wings and tail color dna
+  // and the wings and tail color dna as arguments
+  wingsTailColor(colors[dna.wingsTailColor], dna.wingsTailColor);
+  // Sets the wings and tail color in the wingsTailColor id
+  $("#wingsTailColor").val(dna.wingsTailColor);
 }
 
-// Listening for change
-$('#bodycolor').change(()=>{
-    // Make a color value = to the id body color with the value
-    var colorVal = $('#bodycolor').val()
-    // Call function headColor with the color value in the colors object and color value as arguments
-    headBodyColor(colors[colorVal],colorVal)
+// Create a function to render the eyes with the dna as argument
+function renderEyes(dna) {
+  // Call function eyesColor with the color of the eyes color dna
+  // and the eyes color dna as arguments
+  eyesColor(colors[dna.eyesColor], dna.eyesColor);
+  // Sets the eyes color in the eyesColor id
+  $("#eyesColor").val(dna.eyesColor);
+}
+
+// create a function to render the legs and arms with the dna as argument
+function renderLegsArms(dna) {
+  // Call function legsArmsColor with the color of the legs and arms color dna
+  // and the legs and arms color dna as arguments
+  legsArmsColor(colors[dna.legsArmsColor], dna.legsArmsColor);
+  // Sets the legs and arms dna in the legsArmscolor id
+  $("#legsArmsColor").val(dna.legsArmsColor);
+}
+
+// Listening for change in headBodyColor id
+$("#headBodyColor").change(() => {
+  // Make a color value = to the headBodyColor id with the value
+  var colorVal = $("#headBodyColor").val();
+  // Call function headBodyColor with the colors color value and color value as arguments
+  headBodyColor(colors[colorVal], colorVal);
 })
 
-// Listening for change
-$('#wingsTailColor').change(()=>{
-    // Make a color value = to the id body color with the value
-    var colorVal = $('#wingsTailColor').val()
-    // Call function wingsTailColor with the color value in the colors object and color value as arguments
-    wingsTailColor(colors[colorVal],colorVal)
+// Listening for change in wingsTailColor id
+$("#wingsTailColor").change(() => {
+  // Make a color value = to the wingsTailColor id with the value
+  var colorVal = $("#wingsTailColor").val();
+  // Call function wingsTailColor with the colors color value and color value as arguments
+  wingsTailColor(colors[colorVal], colorVal);
+})
+
+// Listening for change in eyesColor id
+$("#eyesColor").change(() => {
+  // Make a color value = to the eyesColor id with the value
+  var colorVal = $("#eyesColor").val();
+  // Call function eyesColor with the colors color value and color value as arguments
+  eyesColor(colors[colorVal], colorVal);
+})
+
+// Listening for change in legsArmsColor id
+$("#legsArmsColor").change(() => {
+  // Make a color value = to the legsArmsColor id with the value
+  var colorVal = $("#legsArmsColor").val();
+  // Call function legsArmsColor with the colors color value and color value as arguments
+  legsArmsColor(colors[colorVal], colorVal);
 })
